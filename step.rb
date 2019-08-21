4b3bcec1c53c835bd034115b5bcc960d60974ed0
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-08-21 16:06:10 +0200

    exit on exceptions; proper empty checks

--- step.rb
+++ step.rb
@@ -11,16 +11,19 @@ path = ARGV[0]
 scheme = ARGV[1]
 lib_version = ENV['lib_version']
 
-if !path 
+if path.empty?
     puts "Error: BITRISE_PROJECT_PATH env var is required"
+    exit 1
 end
 
-if !scheme
+if scheme.empty?
     puts "Error: BITRISE_SCHEME env var is required"
+    exit 1
 end
 
 if lib_version.empty?
-    raise "Error: missing input LIB_VERSION"
+    puts "Error: missing input lib_version"
+    exit 1
 end
 
 begin
@@ -39,6 +42,7 @@ begin
     helper.link_static_library()
 rescue Exception => e
     puts "Error modifying project to link monitoring library: #{e.message}"
+    exit 1
 end
 
 begin
@@ -46,6 +50,7 @@ begin
     helper.register_resource()
 rescue Exception => e
     puts "Error registering Bitrise configuration plist file: #{e.message}"
+    exit 1
 end
 
 puts "Done!"
